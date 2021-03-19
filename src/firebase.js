e3cc7ac6c4d1bde9c98433246d0a063addf679c2
Sabrina Ales <sabrina-ales@hotmail.com>
2021-03-19 06:14:01 -0400

    config

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,12 @@ import 'firebase/firestore';
 
 // Configuration (mettez-y les vôtres !)
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyA13gQqxNEQ3Fwck3y4U11RuKEMrdHQ8a4",
+  authDomain: "utilisateurs-ex4.firebaseapp.com",
+  projectId: "utilisateurs-ex4",
+  storageBucket: "utilisateurs-ex4.appspot.com",
+  messagingSenderId: "111000932832",
+  appId: "1:111000932832:web:125dcd2b7ce37de4ab9b85"
 };
 
 // Initialiser Firebase
